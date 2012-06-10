c8a579ae64212a3b9ff100a4585e752b9ebc9ed4
Younes <younes.elbiache@gmail.com>
2012-06-10 19:38:45 +0200

    first init

---
+++ README.markdown
@@ -0,0 +1,40 @@
+jQuery Image Magnifier plugin
+=============================
+
+## Description
+
+Displays a magnifying glass (or other image) as an overlay on an image
+
+## Usage
+
+In the header section of your page :
+
+<link href="jquery.imagemagnifier.css" rel="stylesheet">
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="jquery.imagemagnifier.js"></script>
+
+To activate the plugin call `imageMagnifier` method with options on target jQuery object. 
+
+    <script type="text/javascript">
+    $(function(){
+
+    	$("img").imageMagnifier();
+
+    });
+    </script>
+
+## Options
+
+### opacity (integer)
+
+**default**: 0.4
+
+Sets the opacity for the overlay
+
+## Demo 
+
+[Here](http://younes0.github.com/jQuery-Smart-Auto-Complete/demo/index.html)
+
+
+Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) licenses.
+
---
+++ jquery.imagemagnifier.js
@@ -0,0 +1,65 @@
+/**
+ * jQuery imageMagnifier Plugin, displays a magnifying glass (or other image) as an overlay on a image
+ *
+ * Version 1.0
+ *  
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * 
+ */
+(function($) {
+
+	// Start
+    $.imageMagnifier = function(element, options) {
+
+		// Defaults
+       var defaults = {
+       		opacity: 0.4
+       }
+	
+        var plugin = this, $element = $(element);
+		
+		plugin.settings = {}
+		
+		
+        // Constructor
+        plugin.init = function() {
+
+			plugin.settings = $.extend({}, defaults, options);
+						
+			var h = $element.css("height");
+			var w = $element.css("width");		
+
+			$element.wrap('<div class="imageMagnifierWrapper" />')
+
+			$('<div class="imageMagnifier" style="width: '+w+'; height: '+h+'"></div>')
+				.insertBefore($(element))
+				.hover(
+					function () {
+						$(this).stop().animate({opacity: plugin.settings.opacity}, "fast");
+					},
+					function () {
+						$(this).stop().css("opacity", 0);
+					}
+				);
+		
+		},
+
+		// Init
+        plugin.init();
+
+    }
+
+    // Add the plugin to the jQuery.fn object
+    $.fn.imageMagnifier = function(options) {
+        return this.each(function() {
+            if (undefined == $(this).data('imageMagnifier')) {
+                var plugin = new $.imageMagnifier(this, options);
+                $(this).data('imageMagnifier', plugin);
+            }
+        });
+    }
+
+// End
+})(jQuery);
