694ac4144a9c97656ee1f2785787b75b4803e5d9
Younes El Biache <younes.elbiache@gmail.com>
2013-08-30 17:53:16 +0200

    ie fixes

--- jquery.mglass.js
+++ jquery.mglass.js
@@ -59,16 +59,21 @@
 
 			// No CSS3 transition support : javascript fallback
 			if ( ! $.css3Transitions) {
+
+				if (plugin.ieVersion() <= 8) {
+					$overlay.css({"opacity": 0});
+				}
+
 				$overlay.hover(
 					function () {
-						$(this).stop().animate({"opacity": plugin.settings.opacity}, plugin.settings.speed);
+						$(this).css({"opacity": 0}).stop().animate({"opacity": plugin.settings.opacity}, plugin.settings.speed);
 					},
 					function () {
 						$(this).stop().animate({"opacity": 0}, 100);
 					}
 				);
-			}
 
+			}
 		
 		},
 
@@ -89,6 +94,21 @@
 
 		};
 
+		plugin.ieVersion = function(){
+
+			var undef,
+				v = 3,
+				div = document.createElement('div'),
+				all = div.getElementsByTagName('i');
+
+			while (
+				div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+				all[0]
+			);
+
+	    	return v > 4 ? v : undef;
+
+		};
 
 		// Init
 		plugin.init();
